32d926a4377746a4b69f8ee7432566a9f0d66724
Santosh Baggam <santoshbaggam@gmail.com>
2017-01-17 13:08:32 +0530

    first commit

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+	'driver'   => 'mysql',
+
+	'host'     => 'localhost',
+
+	'database' => 'code',
+
+	'username' => 'homestead',
+
+	'password' => 'secret',
+
+	'options' => []
+];
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'vendor/autoload.php';
+
+config('database');
+
+var_dump($config);
